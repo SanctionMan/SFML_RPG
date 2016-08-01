6fc45e96c3fbdf3a1f2dc5f32a8d1349bde39a31
Matthew Alan Flagg <matthew_flagg@live.com>
2016-08-01 17:07:26 -0400

    8/1/16 - update Game class and Main

--- SFML_RPG/Game.cpp
+++ SFML_RPG/Game.cpp
@@ -15,12 +15,25 @@ Game::~Game()
 
 void Game::Run()
 {
+	sf::Clock clock;
+	sf::Time ElapsedTime = clock.getElapsedTime();
+	float FrameTime = 1.0f / 60.0f;
 	while (m_Window.isOpen())
 	{
-		HandleInput();
-		Update();
+		ElapsedTime += clock.restart();
+		while (ElapsedTime.asSeconds() >= FrameTime)
+		{
+			HandleInput();
+			Update(ElapsedTime);
+			ElapsedTime -= sf::seconds(FrameTime);
+		}
 		Render();
 	}
+}
+
+sf::RenderWindow* Game::GetWindow()
+{
+	return &m_Window;
 }
 
 void Game::HandleInput()
@@ -28,13 +41,33 @@ void Game::HandleInput()
 	sf::Event event;
 	while (m_Window.pollEvent(event))
 	{
-		if (event.type == sf::Event::Closed)
+		switch (event.type)
+		{
+		case sf::Event::KeyPressed:
+			handlePlayerInput(event.key.code, true);
+			break;
+		case sf::Event::KeyReleased:
+			handlePlayerInput(event.key.code, false);
+			break;
+		case sf::Event::Closed:
 			m_Window.close();
+			break;
+		}
 	}
 }
 
-void Game::Update()
+void Game::Update(sf::Time ElapsedTime)
 {
+	sf::Vector2f movement(0.f, 0.f);
+	if (m_IsMovingUp)
+		movement.y -= PlayerSpeed;
+	if (m_IsMovingDown)
+		movement.y += PlayerSpeed;
+	if (m_IsMovingLeft)
+		movement.x -= PlayerSpeed;
+	if (m_IsMovingRight)
+		movement.x += PlayerSpeed;
+	m_Player.move(movement * ElapsedTime.asSeconds());
 }
 
 void Game::Render()
@@ -42,4 +75,16 @@ void Game::Render()
 	m_Window.clear();
 	m_Window.draw(m_Player);
 	m_Window.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if (key == sf::Keyboard::W)
+		m_IsMovingUp = isPressed;
+	else if (key == sf::Keyboard::S)
+		m_IsMovingDown = isPressed;
+	else if (key == sf::Keyboard::A)
+		m_IsMovingLeft = isPressed;
+	else if (key == sf::Keyboard::D)
+		m_IsMovingRight = isPressed;
 }
--- SFML_RPG/Game.h
+++ SFML_RPG/Game.h
@@ -9,16 +9,24 @@ public:
 	~Game();
 
 	void Run();
-
+	sf::RenderWindow* GetWindow();
 private:
 
 	void HandleInput();
-	void Update();
+	void Update(sf::Time ElapsedTime);
 	void Render();
+
+	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
 private:
 
 	sf::RenderWindow m_Window;
 	sf::CircleShape m_Player;
 
+	bool m_IsMovingUp;
+	bool m_IsMovingDown;
+	bool m_IsMovingLeft;
+	bool m_IsMovingRight;
+
+	float PlayerSpeed = 100;
 };
