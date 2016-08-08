091230dd3ba5706ebadae6d82553e565e6784dc4
Matthew Alan Flagg <matthew_flagg@live.com>
2016-08-08 06:50:41 -0400

    stuff

--- SFML_RPG/Game.h
+++ SFML_RPG/Game.h
@@ -28,8 +28,8 @@ private:
 	float FrameTime = 1.0f / 60.0f;
 
 	sf::CircleShape m_Player;
-	sf::RectangleShape m_Player2;
-	sf::Sprite texture;
+	sf::Sprite m_Player2;
+	sf::Texture texture;
 
 	bool m_IsMovingUp;
 	bool m_IsMovingDown;
